33bc3ad52098de8a940bc83f3661261410910677
GitHubAnother <1041820154@qq.com>
2016-08-05 19:15:34 +0800

    2

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,65 @@
+﻿/*
+作者名称:YHB
+
+脚本作用:时间
+
+建立时间:2016.8.5.17.32
+*/
+
+using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    #region 字段
+    const float hours = 360 / 12f;//一小时的角度
+    const float minutes = 360 / 60f;//一分钟的角度
+    const float seconds = 360 / 60f;//一秒钟的角度
+
+    public Toggle toggle;
+
+    private Transform hour, minute, second;//外部拖入的时针分针秒针
+    private bool isAnalog = false;
+    #endregion
+
+    #region Unity内置函数
+    void Awake()
+    {
+        hour = this.transform.FindChild("HourCentre").transform;
+        minute = this.transform.FindChild("MnuterCentre").transform;
+        second = this.transform.FindChild("SecondCentre").transform;
+
+        toggle.isOn = false;
+    }
+    void Update()
+    {
+        isAnalog = toggle.isOn;
+
+        NowTimeRotation();
+    }
+    #endregion
+
+    #region 根据当前时间转动对应的时针分针秒针
+    void NowTimeRotation()
+    {
+        DateTime time = DateTime.Now;//获取系统当前的时间
+
+        if (!isAnalog)
+        {
+            hour.localRotation = Quaternion.Euler(0, -time.Hour * hours, 0);
+            minute.localRotation = Quaternion.Euler(0, -time.Minute * minutes, 0);
+            second.localRotation = Quaternion.Euler(0, -time.Second * seconds, 0);
+        }
+        else
+        {
+            TimeSpan timeSpan = DateTime.Now.TimeOfDay;//更加精细的时间
+
+            hour.localRotation = Quaternion.Euler(0, -(float)timeSpan.TotalHours * hours, 0);
+            minute.localRotation = Quaternion.Euler(0, -(float)timeSpan.TotalMinutes * minutes, 0);
+            second.localRotation = Quaternion.Euler(0, -(float)timeSpan.TotalSeconds * seconds, 0);
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Time.cs
@@ -0,0 +1,88 @@
+﻿/*
+作者名称:YHB
+
+脚本作用:电子时钟
+
+建立时间:2016.8.5.18.50
+*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class Time : MonoBehaviour
+{
+    #region 字段
+    private Text h;
+    private Text m;
+    private Text s;
+    #endregion
+
+    #region Unity内置函数
+    void Awake()
+    {
+        h = this.transform.FindChild("H").GetComponent<Text>();
+        m = this.transform.FindChild("M").GetComponent<Text>();
+        s = this.transform.FindChild("S").GetComponent<Text>();
+    }
+    void Update()
+    {
+        TimeUpdate();
+    }
+    #endregion
+
+    #region 计时
+    void TimeUpdate()
+    {
+        DateTime time = DateTime.Now;
+
+        int hour = time.Hour;
+        int minute = time.Minute;
+        int second = time.Second;
+
+        if (hour < 10 && hour > 0)
+        {
+            h.text = "0" + time.Hour;
+        }
+        else if (hour > 9)
+        {
+            h.text = time.Hour.ToString();
+        }
+        else
+        {
+            h.text = "00";
+        }
+
+        if (minute < 10 && minute > 0)
+        {
+            m.text = "0" + time.Minute;
+        }
+        else if (hour > 9)
+        {
+            m.text = time.Minute.ToString();
+        }
+        else
+        {
+            m.text = "00";
+        }
+
+        if (second < 10 && second > 0)
+        {
+            s.text = "0" + time.Second;
+        }
+        else if (second > 9)
+        {
+            s.text = time.Second.ToString();
+        }
+        else
+        {
+            s.text = "00";
+        }
+
+        /*h.text = time.Hour.ToString();
+        m.text = time.Minute.ToString();
+        s.text = time.Second.ToString();*/
+    }
+    #endregion
+}
